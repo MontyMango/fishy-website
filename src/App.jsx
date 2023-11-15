28a81b1b6c1e13e51a3ecdbdd594957ab34ef1c7
NathanKVB <132923405+NathanKVB@users.noreply.github.com>
2023-11-15 10:51:55 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import Waves from "/Assets/waves.jpg";
 import Bass from "/Assets/bass.png";
 import Crab from "/Assets/a_crab.png";
 import ClownFish from "/Assets/clownfish.png";
@@ -8,27 +9,74 @@ import Shark3D from "./components/Shark3D";
 import Crab3D from "./components/Crab3D";
 
 function App() {
+  const [array, setArray] = useState([0, 1, 2, 3, 4, 5]);
+
   return (
     <>
-      {/* <img src={Waves} alt="Waves" style={{ width: '100%', height: '800px' }} /> */}
-      <div className="gradient-background">
-        <p>Dive Below</p>
-        <div>
-          <Link to="/Bass">
-            <img src={Bass} alt="Bass" />
-          </Link>
-          <Link to="/Crab">
-            <img src={Crab} alt="Crab" />
-          </Link>
-          <Link to="/ClownFish">
-            <img src={ClownFish} alt="ClownFish" />
-          </Link>
-        </div>
-        {/* <div style={{ display: "flex", maxWidth: "100vw", overflow: "auto" }}>
-          <div style={{ width: "50%" }}>
-            <Shark3D />
+      {/* Body, where the fish goes */}
+      <div className="container water" style={{ paddingTop: "15%" }}>
+        {/* Fish #1: Crab */}
+        {array.map(() => {
+          return (
+            <div className="row">
+              {/*Title Column*/}
+              <div className="col-lg-12">
+                <h1>
+                  This is the <a href="">crab</a>
+                </h1>
+              </div>
+              {/*Photo & Description Column */}
+              <div className="row">
+                {/* Left Photo Column*/}
+                <div className="col-lg-6 img">
+                  <a href="/l">
+                    <img src="/Assets/a_crab.png" alt="A crab" />
+                  </a>
+                </div>
+                {/* Right Description Column */}
+                <div className="col-lg-6">
+                  <p>
+                    This crab sometimes crawls on the beach and sometimes
+                    pinches people.
+                  </p>
+                  <h5>
+                    <a href="/">Learn more about the crab!</a>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        {/* Fish #2: Clownfish */}
+        <div className="row">
+          {/*Title Column*/}
+          <div className="col-lg-12" style={{ float: "right" }}>
+            <h1>
+              This is the <a href="">clownfish</a>
+            </h1>
           </div>
-        </div> */}
+          {/* Photo & Description Column */}
+          <div className="row">
+            {/* Left Description Column */}
+            <div className="col-lg-6" style={{ paddingLeft: "9%" }}>
+              <p>
+                This fish looks cool, but I don't know if they can breath in
+                that water.
+              </p>
+              <h5>
+                <a href="/clownFish">Learn more about the clownfish!</a>
+              </h5>
+            </div>
+            <div className="row">
+              {/* Right Photo Column*/}
+              <div className="col-lg-10" style={{ paddingLeft: "10%" }}>
+                <img src="/Assets/clownfish.png" alt="Picture of a clownfish" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* OptionaJavaScript */}
+        {/* jQuery first, then Popper.js, then Bootstrap JS */}
       </div>
     </>
   );
